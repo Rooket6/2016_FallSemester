29da0f97c3b0accfd25c4cf26d62d35f9ffe23c4
Tyler Wasniowski <Rooket6@yahoo.com>
2016-09-08 00:23:11 -0700

    Initial commit

---
+++ AppointmentBook/src/mainTab/Frame.java
@@ -0,0 +1,15 @@
+package mainTab;
+
+import javax.swing.JPanel;
+
+public class Frame {
+	
+	public JPanel createContentPane() {
+		
+		JPanel contentPane = new JPanel();
+		
+		return contentPane;
+		
+	}
+	
+}
