4486cbb88974366e6df5dcdb85014c75bc7b4d6c
Tyler Wasniowski <Rooket6@yahoo.com>
2016-09-09 01:08:09 -0700

    Finished for the night, shows days of month and of week

--- AppointmentBook/src/mainTab/AppointmentBookWindow.java
+++ AppointmentBook/src/mainTab/AppointmentBookWindow.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
@@ -21,39 +23,41 @@ import javax.swing.SwingConstants;
 import appointmentBookController.AppointmentBookController;
 
 public class AppointmentBookWindow implements ActionListener {
-	
-	// For the starting size of the calendar
-	private int calendarWidth = 200;
-	private int calendarHeight = 450;
+
+	private int buttonHeight = 30;
+	private int calendarHeight = 550;
 	
 	// The controller acts as a calendar and also handles appointments
 	private AppointmentBookController controller;
 
+	private JPanel contentPane;
 	private CalendarComponent calendarComponent; // The visual component of the calendar
-	private JPanel headerComponent;
-	private JButton leftButton, rightButton, createAppointment;
+	private JPanel headerComponent, footerComponent; // The buttons and label above and below calendar
+	private JButton leftButton, rightButton, makeAppointmentButton;
 	private JLabel headerLabel;
 	
 	public AppointmentBookWindow() {
 		
 		controller = new AppointmentBookController();
-		calendarComponent = new CalendarComponent();
 		
 	}
 	
-	private JPanel createContentPane() {
+	private void createContentPane() {
 		
-		JPanel contentPane = new JPanel();
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		contentPane = new JPanel();
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS)); // Infinitely wide, dynamically high
 		
 		createHeaderComponent();
-		headerComponent.setMaximumSize(new Dimension(Short.MAX_VALUE, 60)); // Infinitely wide, but confined to 60px high
+		headerComponent.setMaximumSize(new Dimension(Short.MAX_VALUE, buttonHeight)); // Infinitely wide, but confined to 60px high
 		contentPane.add(headerComponent);
-		
-		calendarComponent.setSize(300, 450);
+
+		calendarComponent = new CalendarComponent();
+		calendarComponent.setMaximumSize(new Dimension(Short.MAX_VALUE, calendarHeight + 5));
 		contentPane.add(calendarComponent);
 		
-		return contentPane;
+		createFooterComponent();
+		footerComponent.setMaximumSize(new Dimension(Short.MAX_VALUE, buttonHeight + 5));
+		contentPane.add(footerComponent);
 		
 	}
 	
@@ -64,13 +68,13 @@ public class AppointmentBookWindow implements ActionListener {
 		headerComponent.setLayout(new BorderLayout());
 		
 		leftButton = new JButton(" < ");
-		leftButton.addActionListener(this);
+		leftButton.addActionListener(this); // Triggers event when clicked
 		headerComponent.add(leftButton, BorderLayout.WEST);
 		
 		// Shows the current month and year
 		headerLabel = new JLabel(controller.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + ", " + controller.get(Calendar.YEAR));
 		headerLabel.setHorizontalAlignment(JLabel.CENTER);
-		headerComponent.add(headerLabel);
+		headerComponent.add(headerLabel, BorderLayout.CENTER);
 		
 		rightButton = new JButton(" > ");
 		rightButton.addActionListener(this);
@@ -80,11 +84,12 @@ public class AppointmentBookWindow implements ActionListener {
 	
 	private void createFooterComponent() {
 		
-	}
-	
-	private void updateCalendarComponent() {
+		footerComponent = new JPanel();
 		
-//		calendarComponent.cell
+		makeAppointmentButton = new JButton("Make Appointment");
+		makeAppointmentButton.setMaximumSize(new Dimension(Short.MAX_VALUE, buttonHeight));
+		makeAppointmentButton.addActionListener(this);
+		footerComponent.add(makeAppointmentButton);
 		
 	}
 	
@@ -94,17 +99,27 @@ public class AppointmentBookWindow implements ActionListener {
 		headerLabel.setText(controller.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + ", " + controller.get(Calendar.YEAR));
 		
 	}
-	
-	private String[] getDaysOfWeek() {
-		
-		String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-		return daysOfWeek;
-		
-	}
-	
 	private String[][] getDaysOfMonth() {
 		
 		String[][] daysOfMonth = new String[6][7];
+		
+		int startDay = controller.get(Calendar.DAY_OF_WEEK) - 1; // -1 so Sunday is 0
+		int endDay = controller.getActualMaximum(Calendar.DAY_OF_MONTH);
+		
+		int dayCounter = 1; // Keeps track of day of month
+		for (int r = 0; r < daysOfMonth.length; r++) {
+			for (int c = 0; c < daysOfMonth[0].length; c++) {
+				
+				if (r != 0 || c >= startDay) {
+					daysOfMonth[r][c] = Integer.toString(dayCounter);
+					dayCounter++;
+				}
+				if (dayCounter > endDay) {
+					return daysOfMonth;
+				}
+				
+			}
+		}
 		
 		return daysOfMonth;
 		
@@ -114,10 +129,11 @@ public class AppointmentBookWindow implements ActionListener {
 		
 		JFrame frame = new JFrame("Appointment Book");
 		
-		frame.setContentPane(createContentPane());
+		createContentPane();
+		frame.setContentPane(contentPane);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setMinimumSize(new Dimension(250, 550));
+		frame.setMinimumSize(new Dimension(550, 2*buttonHeight + calendarHeight + 49)); // 550 and 49 are arbitrary for visual appeal
 		frame.pack();
 		frame.setVisible(true);
 				
@@ -130,13 +146,13 @@ public class AppointmentBookWindow implements ActionListener {
 			
 			controller.add(Calendar.MONTH, -1);
 			updateHeaderLabel();
-			System.out.println("Left button fired");
+			calendarComponent.updateCalendarDays();
 			
 		} else if (e.getSource() == rightButton) {
 			
 			controller.add(Calendar.MONTH, 1);
 			updateHeaderLabel();
-			System.out.println("Right button fired");
+			calendarComponent.updateCalendarDays();
 			
 		}
 		
@@ -145,15 +161,87 @@ public class AppointmentBookWindow implements ActionListener {
 	@SuppressWarnings("serial")
 	private class CalendarComponent extends JPanel {
 		
-		private String[] daysOfWeek = getDaysOfWeek();
+		String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		
+		private JLabel[] daysOfWeekLabels;
+		private JLabel[][] daysOfMonthLabels;
+		
+		public CalendarComponent() {
+			
+			super();
+			this.setLayout(new GridLayout(7,7));
+			
+			daysOfWeekLabels = new JLabel[7];
+			daysOfMonthLabels = new JLabel[6][7];
+			
+			populateDaysOfWeek();
+			populateDaysOfMonth();
+			
+		}
+		
+		private void populateDaysOfWeek() {
+			
+			for(int i = 0; i < daysOfWeekLabels.length; i++) {
+				
+				daysOfWeekLabels[i] = new JLabel(daysOfWeek[i]);
+				this.add(daysOfWeekLabels[i]);
+				
+			}
+			
+		}
+		
+		private void populateDaysOfMonth() {
+			
+			String[][] daysOfMonth = getDaysOfMonth();
+			
+			for (int r = 0; r < daysOfMonthLabels.length; r++) {
+				for (int c = 0; c < daysOfMonthLabels[0].length; c++) {
+					
+					daysOfMonthLabels[r][c] = null;
+					if (daysOfMonth[r][c] != null) {
+
+						daysOfMonthLabels[r][c] = new JLabel(daysOfMonth[r][c]);
+						this.add(daysOfMonthLabels[r][c]);
+						
+					}
+					
+				}
+			}
+			
+		}
+		
+		public void updateCalendarDays() {
+			
+			this.removeAll();
+			populateDaysOfWeek();
+			populateDaysOfMonth();
+			
+		}
 		
 		@Override
 		protected void paintComponent(Graphics g) {
 			
 			super.paintComponent(g);
 			
+			// White background
 			g.setColor(Color.WHITE);
-			g.fillRect(0, 0, Integer.MAX_VALUE, 450);
+			g.fillRect(0, 0, Short.MAX_VALUE, calendarHeight);
+			
+			// Grid
+			g.setColor(Color.BLACK);
+			// Vertical Lines
+			int horizontalSpaceBetweenLines = this.getWidth() / 7;
+			for (int i = 1; i < 7; i++) {
+				g.drawLine(i * horizontalSpaceBetweenLines, 0, i * horizontalSpaceBetweenLines, calendarHeight);
+			}
+			// Horizontal Lines
+			g.drawLine(0, 0, this.getWidth(), 0);
+			g.drawLine(0, 30, this.getWidth(), 30);
+			int verticalSpaceBetweenLines = (calendarHeight - 30) / 7; // Starts lower because of weekday names column
+			for (int i = 0; i < 8; i++) {
+				g.drawLine(0, i * verticalSpaceBetweenLines + 30, this.getWidth(), i * verticalSpaceBetweenLines + 30);
+			}
+			
 			
 		}
 		
