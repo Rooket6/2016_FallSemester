6e679500d9fbca202bbdfa619263872287e727f0
Tyler Wasniowski <Rooket6@yahoo.com>
2016-09-14 14:07:17 -0700

    2:10PM

--- AppointmentBook/src/mainTab/AppointmentBookWindow.java
+++ AppointmentBook/src/mainTab/AppointmentBookWindow.java
@@ -2,33 +2,38 @@ package mainTab;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 import appointmentBookController.AppointmentBookController;
 
-public class AppointmentBookWindow implements ActionListener {
+@SuppressWarnings("serial")
+public class AppointmentBookWindow extends JFrame implements ActionListener {
 
 	private int buttonHeight = 30;
 	private int calendarHeight = 550;
+	
+	String[][] daysOfMonth;
 	
 	// The controller acts as a calendar and also handles appointments
 	private AppointmentBookController controller;
@@ -40,8 +45,13 @@ public class AppointmentBookWindow implements ActionListener {
 	private JLabel headerLabel;
 	
 	public AppointmentBookWindow() {
+
+		super();
 		
+		daysOfMonth  = new String[6][7];
 		controller = new AppointmentBookController();
+		
+		updateDaysOfMonth();
 		
 	}
 	
@@ -102,10 +112,10 @@ public class AppointmentBookWindow implements ActionListener {
 		headerLabel.setText(controller.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + ", " + controller.get(Calendar.YEAR));
 		
 	}
-	private String[][] getDaysOfMonth() {
-		
-		String[][] daysOfMonth = new String[6][7];
-		
+	
+	private void updateDaysOfMonth() {
+
+		// Where to start and where to stop listing calendar numbers
 		int startDay = controller.get(Calendar.DAY_OF_WEEK) - 1; // -1 makes it so Sunday is 0
 		int endDay = controller.getActualMaximum(Calendar.DAY_OF_MONTH);
 		
@@ -113,33 +123,29 @@ public class AppointmentBookWindow implements ActionListener {
 		for (int r = 0; r < daysOfMonth.length; r++) {
 			for (int c = 0; c < daysOfMonth[0].length; c++) {
 				
-				if (r != 0 || c >= startDay) {
+				// starts and ends at populating calendar numbers at appropriate times
+				if ((r != 0 || c >= startDay) && (dayCounter <= endDay)) {
 					daysOfMonth[r][c] = Integer.toString(dayCounter);
 					dayCounter++;
-				}
-				if (dayCounter > endDay) {
-					return daysOfMonth;
+				} else {
+					daysOfMonth[r][c] = null;
 				}
 				
 			}
 		}
 		
-		return daysOfMonth;
-		
 	}
 	
 	private void createAndShowGUI() {
 		
-		JFrame frame = new JFrame("Appointment Book");
-		
 		createContentPane();
-		frame.setContentPane(contentPane);
+		setContentPane(contentPane);
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setMinimumSize(new Dimension(600, 2*buttonHeight + calendarHeight + 49)); // 550 and 49 are arbitrary for visual appeal
-		frame.setPreferredSize(new Dimension(800, frame.getMinimumSize().height)); // 650 arbitrary, height is minimum height
-		frame.setSize(frame.getPreferredSize());
-		frame.setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(600, 2*buttonHeight + calendarHeight + 49)); // 550 and 49 are arbitrary for visual appeal
+		setPreferredSize(new Dimension(800, getMinimumSize().height)); // 650 arbitrary, height is minimum height
+		setSize(getPreferredSize());
+		setVisible(true);
 				
 	}
 
@@ -163,8 +169,31 @@ public class AppointmentBookWindow implements ActionListener {
 			}
 			
 			updateHeaderLabel();
-			calendarComponent.updateCalendarDays();
+			updateDaysOfMonth();
+			calendarComponent.repopulateDaysOfMonth();
 			calendarComponent.repaint();
+			
+		} else if (e.getSource() == makeAppointmentButton) {
+			
+			MakeAppointmentPanel makeAppointmentPanel = new MakeAppointmentPanel();
+			
+			Object[] message = {new MakeAppointmentPanel()};
+			
+		    int userOption = JOptionPane.showConfirmDialog(this,
+		    		message,
+		    		"Make Appointment",
+		    		JOptionPane.OK_CANCEL_OPTION
+		    );
+		    
+		    if (userOption ==  JOptionPane.OK_OPTION) {
+		    	
+		    	// TODO: Implement selected type and custom description
+		    	controller.addAppointment(0, "Appointment", makeAppointmentPanel.getMonth(), makeAppointmentPanel.getDay(), makeAppointmentPanel.getYear());
+		    	
+		    }
+
+			
+//			controller.addAppointment(type, description, month, day, year);
 			
 		}
 		
@@ -182,7 +211,7 @@ public class AppointmentBookWindow implements ActionListener {
 			this.setBackground(Color.WHITE);
 			this.setLayout(new GridBagLayout());
 			
-			updateCalendarDays();
+			repopulateDaysOfMonth();
 			
 		}
 		
@@ -244,7 +273,7 @@ public class AppointmentBookWindow implements ActionListener {
 			
 		}
 		
-		public void updateCalendarDays() {
+		public void repopulateDaysOfMonth() {
 			
 			this.removeAll();
 			populateDaysOfWeek();
@@ -275,11 +304,202 @@ public class AppointmentBookWindow implements ActionListener {
 		
 	}
 	
+	// Panel that appears when "makeAppointmentButton" is pressed
+	private class MakeAppointmentPanel extends JPanel implements ActionListener, FocusListener {
+		
+		private GregorianCalendar calendar;
+		
+		private JPanel monthPanel;
+		private JLabel monthLabel;
+		private JComboBox<String> monthDropDown;
+		
+		private JPanel dayPanel;
+		private JLabel dayLabel;
+		private JComboBox<String> dayDropDown;
+		
+		private JPanel yearPanel;
+		private JLabel yearLabel;
+		private JTextField yearTextField;
+		
+		private final String[] MONTHS = {"Janurary", "Feburary", "March", "April", "June", "July", "August", "September", "October", "November", "December"};
+		
+		public MakeAppointmentPanel() {
+			
+			setLayout(new BorderLayout());
+			
+			calendar = new GregorianCalendar();
+			calendar.set(Calendar.MONTH, Calendar.JANUARY);
+			
+			monthPanel = new JPanel();
+			
+			monthLabel = new JLabel("Month: ");
+			monthPanel.add(monthLabel, BorderLayout.LINE_START);
+			
+			monthDropDown = new JComboBox<String>();
+			for (String month : MONTHS) {
+				monthDropDown.addItem(month);
+			}
+			monthDropDown.addActionListener(this);
+			monthPanel.add(monthDropDown, BorderLayout.LINE_END);
+			
+			add(monthPanel, BorderLayout.LINE_START);
+			
+	
+			dayPanel = new JPanel();
+			
+			dayLabel = new JLabel("Day: ");
+			dayPanel.add(dayLabel, BorderLayout.LINE_START);
+			
+			dayDropDown = new JComboBox<String>();
+			populateDayDropDown();
+			dayDropDown.addActionListener(this);
+			dayPanel.add(dayDropDown, BorderLayout.LINE_END);
+			
+			add(dayPanel, BorderLayout.CENTER);
+
+			
+			yearPanel = new JPanel();
+			
+			yearLabel = new JLabel("Year: ");
+			yearPanel.add(yearLabel, BorderLayout.LINE_START);
+			
+			yearTextField = new JTextField();
+			yearTextField.setText(Integer.toString(calendar.get(Calendar.YEAR)));
+			yearTextField.addFocusListener(this);
+			yearPanel.add(yearTextField, BorderLayout.LINE_END);
+			
+			add(yearPanel, BorderLayout.LINE_END);
+			
+		}
+		
+		private void populateDayDropDown() {
+			
+			dayDropDown.removeAllItems();
+			
+			int daysInSelectedMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+			
+			for (int n = 1; n <= daysInSelectedMonth; n++)
+				dayDropDown.addItem(Integer.toString(n));
+			
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// If selected month changed, update calendar month so "updateDayDropDown" will get the accurate number of days in month
+			if (e.getSource() == monthDropDown) {
+
+				String selectedMonth = (String) monthDropDown.getSelectedItem();
+
+				switch (selectedMonth) {
+				
+					case "Janurary":
+						calendar.set(Calendar.MONTH, Calendar.JANUARY);
+						break;
+					case "Feburary":
+						calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+						break;
+					case "March":
+						calendar.set(Calendar.MONTH, Calendar.MARCH);
+						break;
+					case "April":
+						calendar.set(Calendar.MONTH, Calendar.APRIL);
+						break;
+					case "May":
+						calendar.set(Calendar.MONTH, Calendar.MAY);
+						break;
+					case "June":
+						calendar.set(Calendar.MONTH, Calendar.JUNE);
+						break;
+					case "July":
+						calendar.set(Calendar.MONTH, Calendar.JULY);
+						break;
+					case "August":
+						calendar.set(Calendar.MONTH, Calendar.AUGUST);
+						break;
+					case "September":
+						calendar.set(Calendar.MONTH, Calendar.SEPTEMBER);
+						break;
+					case "October":
+						calendar.set(Calendar.MONTH, Calendar.OCTOBER);
+						break;
+					case "November":
+						calendar.set(Calendar.MONTH, Calendar.NOVEMBER);
+						break;
+					case "December":
+						calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+						break;
+				
+				}
+				
+				populateDayDropDown();				
+				
+			} else if (e.getSource() == dayDropDown) {
+				
+				int selectedDay = Integer.parseInt((String) dayDropDown.getSelectedItem());
+				
+				calendar.set(Calendar.DAY_OF_MONTH, selectedDay);
+				
+			}
+			
+		}
+
+		@Override
+		public void focusLost(FocusEvent e) {
+
+			if (e.getSource() == yearTextField) {
+				
+				int selectedYear;
+				
+				try {
+					
+					selectedYear = Integer.parseInt(yearTextField.getText());
+					
+					if (selectedYear < 0)
+						throw new NumberFormatException();
+					
+				} catch (NumberFormatException ex) {
+					
+					JOptionPane.showMessageDialog(this, "Must enter a positive integer");
+					return;
+					
+				}
+				
+				calendar.set(Calendar.YEAR, selectedYear);
+				
+				populateDayDropDown();
+				
+			}
+			
+		}
+		
+		public int getMonth() {
+			return calendar.get(Calendar.MONTH);
+		}
+		
+		public int getDay() {
+			return calendar.get(Calendar.DAY_OF_MONTH);
+		}
+		
+		public int getYear() {
+			return calendar.get(Calendar.YEAR);
+		}
+
+		@Override
+		public void focusGained(FocusEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private String[][] getDaysOfMonth() {
+		return daysOfMonth;
+	}
+	
 	public static void main(String[] args) {
 		
 		AppointmentBookWindow frame = new AppointmentBookWindow();
 		frame.createAndShowGUI();
-		
 		
 	}
 	
